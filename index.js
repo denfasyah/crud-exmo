10400ded8f0fae1b8288ac7083c9fff05e1bc3a8
denfasyah <denfasyah@gmail.com>
2023-12-09 14:42:40 +0700

    add product

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ mongoose.connect('mongodb://127.0.0.1/ecommerce_db').then((result) => {
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.use(express.urlencoded({extended: true}));
 
 app.get('/', (req, res) => {
     res.render('home/index')
@@ -24,11 +25,22 @@ app.get('/products', async (req, res) => {
     res.render('products/index',{products})
 });
 
+app.get('/products/create',(req, res) => {
+    res.render('products/create')
+})
+
+app.post('/products', async(req, res) => {
+    const product = new Product(req.body)
+    await product.save()
+    res.redirect(`/products/${product._id}`)
+})
+
 app.get('/products/:id', async (req, res) => {
     const {id} = req.params
     const product = await Product.findById(id)
     res.render('products/show',{product})
 })
+
 
 app.listen(3000, () => {
     console.log('http://127.0.0.1:3000');
